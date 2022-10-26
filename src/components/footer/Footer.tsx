bc03f832990b01d36a17b02850ae2d4ab910ea47
Raul Nascimento <raul.hnr23@gmail.com>
2022-10-25 23:34:27 -0300

    Footer update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,37 @@
+import { Link } from "@mui/material";
 import React from "react";
 import './Footer.css';
+import instagramIcon from "../../assets/icon_instagram.png";
+import facebookIcon from "../../assets/icon_facebook.png";
+import linkedinIcon from "../../assets/icon_linkedin.png";
 
 export const Footer = () => {
     return (
         <footer>
-            
+            <section className="footer-info">
+                <div className="footer-info-sec">
+                    <h3>Contato</h3>
+                    <p>(99) 9 9999-9999</p>
+                    <p>contato@mail.com.br</p>
+                    <p>Rua Fulana de Tal, 999 - Vila Campinas, São Paulo - SP</p>
+                </div>
+                <div className="footer-info-sec">
+                    <h3>Social</h3>
+                    <Link href="" underline="none">
+                        <img src={instagramIcon} alt="Instagram" className="icon" />
+                        <p>Instagram</p>
+                    </Link>
+                    <Link href="" underline="none">
+                    <img src={facebookIcon} alt="Facebook" className="icon" />
+                        <span>Facebook</span>
+                    </Link>
+                    <Link href="" underline="none">
+                    <img src={linkedinIcon} alt="Linkedin" className="icon" />
+                        <span>LinkedIn</span>
+                    </Link>
+                </div>
+            </section>
+            <p>Copyright© A&C Advogados 2022</p>
         </footer>
     )
 }
